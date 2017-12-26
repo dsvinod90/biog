16187914bd4f8fe128a92cc82744e5d70d8600c9
VinodDalavai <dsvinod.1990@icloud.com>
2017-12-26 16:43:17 +0530

    Add news section

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 	root 'welcome#index'
+  resources :articles, path: '/blog/articles'
   resources :welcome, only: [:index] do
     collection do
       get :about_me
     end
   end
-  resources :articles, path: '/blog/articles'
   devise_for :admin, controllers: {
         sessions: 'admin/sessions'
       }
+  namespace :api do
+    resources :news, only: [:index]
+  end 
 end
